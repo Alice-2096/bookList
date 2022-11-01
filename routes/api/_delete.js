d9f2ae51abc36e30d7a52e5b929caadc79c4c4a4
Alice <xinranjiang2015@yahoo.com>
2022-11-01 19:21:37 -0400

    fixed bugs

--- routes/api/_delete.js
+++ routes/api/_delete.js
@@ -3,8 +3,6 @@ import { deleteBook } from '../../controllers/book.js';
 export default async (req, res) => {
   try {
     const bookId = req.params.id;
-    console.log('🚀 ~ file: _delete.js ~ line 6 ~ bookId', bookId);
-
     deleteBook(bookId);
   } catch (error) {
     console.log(error);
