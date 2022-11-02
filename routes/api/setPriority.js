cbf8b070651cf528e87fc80112eb98587260a9a4
Alice <xinranjiang2015@yahoo.com>
2022-11-02 15:22:03 -0400

    fixed homepage views

--- routes/api/setPriority.js
+++ routes/api/setPriority.js
@@ -1,12 +1,13 @@
 import { setBookPriority } from '../../controllers/book.js';
+import { sortByPriority } from '../../controllers/book.js';
 
 export default async (req, res) => {
   try {
     const bookId = req.params.id;
     const { priority } = req.body;
-
     await setBookPriority(bookId, priority);
     //sort books in DB
+    sortByPriority();
   } catch (error) {
     console.log(error);
   }
